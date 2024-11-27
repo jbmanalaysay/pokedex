ef802db30a2307c5a426e1f6aff9ba5eab825884
jaymarmanalaysay <jaymarmanalaysay@gmail.com>
2024-11-28 07:06:55 +0800

    feat: added evolutionChain.js and getPokemon

--- scripts/getEvolutionChain.js
+++ scripts/getEvolutionChain.js
@@ -2,11 +2,30 @@ import config from '../config.js'
 import getApi from './getRequestApi.js'
 
 const getEvolution = async (pokemonName) => {
-    const speciesUrl = config.endpoints.baseEndpoint +'pokemon-species/' + pokemonName;
+    const speciesUrl = config.endpoints.baseEndpoint + 'pokemon-species/' + pokemonName;
     const speciesJson = await getApi(speciesUrl);
     const evolutionChainUrl = speciesJson.evolution_chain.url;
-    const evolutionChainJson = await getApi(evolutionChainUrl);
-    return evolutionChainJson;
+    let evolutionChainJson = await getApi(evolutionChainUrl);
+    // Create Evolution Object
+    let evolutionTree = {
+        name: evolutionChainJson.chain.species.name,
+        isBaby: evolutionChainJson.chain.is_baby,
+        evolvesTo:[]
+    };
+
+    let evolvesTo = evolutionChainJson.chain.evolves_to;
+    evolutionTree.evolvesTo = transformEvolution(evolvesTo);
+    
+    return JSON.stringify(evolutionTree);
 }
 
-(async () => console.log( await getEvolution('wurmple')))();
+(async () => console.log(await getEvolution('eevee')))();
+
+
+const transformEvolution = (evolvesTo) => {
+    
+    return evolvesTo.map(evolveStage =>( {
+        name: evolveStage.species.name,
+        evolvesTo: transformEvolution(evolveStage.evolves_to)
+    }));
+}
--- scripts/getPokemon.js
+++ scripts/getPokemon.js
@@ -1,4 +1,10 @@
+import config from '../config.js'
+import getApi from './getRequestApi.js'
 
-const getPokemon = (pokemonName) => {
-    const pokemonUrl = 'https://pokeapi.co/api/v2/'
+const getPokemon = async (pokemonName) => {
+    const pokemonUrl = config.endpoints.baseEndpoint + 'pokemon/' + pokemonName;
+    const pokemonJson = await getApi(pokemonUrl);
+    console.log(pokemonJson);
 }
+
+(async()=>{console.log(getPokemon('bulbasaur'));})()
