e64bfbf92494d5e040cbf3830e36e6af8c01483c
Jaymar Manalaysay <jaymar.manalaysay@actimai.com>
2024-11-27 21:20:23 +0800

    feat: create evolution object for the pokemon

---
+++ sampleEvolution.js
@@ -0,0 +1,19 @@
+const evolution = [
+    {
+        name: 'wurmple',
+        isBaby: false,
+        evolvesTo: [
+            {
+                name: 'silicoon',
+                evolveTrigger: 'Level-Up up to Level 7',
+                evolvesTo: [
+                    {
+                        name: 'silicoon',
+                        evolveTrigger: 'Level-Up up to Level 7',
+                        evolvesTo:
+                    }
+                ]
+            }
+        ]
+    }
+];
--- scripts/getEvolutionChain.js
+++ scripts/getEvolutionChain.js
@@ -1,2 +1,12 @@
-import config from '...'
+import config from '../config.js'
+import getApi from './getRequestApi.js'
 
+const getEvolution = async (pokemonName) => {
+    const speciesUrl = config.endpoints.baseEndpoint +'pokemon-species/' + pokemonName;
+    const speciesJson = await getApi(speciesUrl);
+    const evolutionChainUrl = speciesJson.evolution_chain.url;
+    const evolutionChainJson = await getApi(evolutionChainUrl);
+    return evolutionChainJson;
+}
+
+(async () => console.log( await getEvolution('wurmple')))();
